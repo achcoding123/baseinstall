fb02ea8e7ce17824cfdf262a2bfb004e511f2882
Аш <podrabotkascam@gmail.com>
2024-09-26 06:26:05 +0300

    Add files via upload

---
+++ baseinstall.sh
@@ -0,0 +1,26 @@
+pacman -S neofetch
+echo "тестируем neofetch"
+neofetch
+echo "neofetch работает! устанавливаем firefox btw"
+pacman -S firefox
+echo "тестируем firefox"
+firefox
+echo "firefox работает! устанавливаем всякие прихолюхи по типу cmatrix и cowsay"
+pacman -S cowsay
+cowsay тест
+echo "если вы увидили текст который говорит корова значит cowsay установился"
+echo "устанавливаем cmatrix"
+pacman -S cmatrix
+echo "cmatrix установлен!"
+echo "устанавливаем discord"
+pacman -S discord
+echo "тестируем discord"
+discord
+echo "discord установлен!"
+echo "устанавливаем Telegram"
+pacman -S telegram-desktop
+echo "Telegram Установлен!"
+echo "все базовые программы установлены!"
+echo "поддержать автора: https://boosty.to/achach/donate"
+echo "спасибо за использование программы!"
+
